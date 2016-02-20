d7ab3455233d8ee029b71e098010295e77e1ca91
d33play <r.a.abdullayev@gmail.com>
2016-02-21 01:33:09 +0400

    Create MainActivity class and layout

---
+++ app/src/main/java/az/deep/remindme/MainActivity.java
@@ -0,0 +1,17 @@
+package az.deep.remindme;
+
+import android.app.Activity;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+
+/**
+ * Created by rashad on 2016-02-21.
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
