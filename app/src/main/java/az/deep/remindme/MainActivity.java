a7389e2aa4f5d0e7de8c08d6030e9a47648c521f
d33play <r.a.abdullayev@gmail.com>
2016-02-21 17:38:03 +0400

    [RM-3] Add Toolbar and search button

--- app/src/main/java/az/deep/remindme/MainActivity.java
+++ app/src/main/java/az/deep/remindme/MainActivity.java
@@ -1,17 +1,36 @@
 package az.deep.remindme;
 
 import android.app.Activity;
-import android.content.pm.ActivityInfo;
+//import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
-/**
- * Created by rashad on 2016-02-21.
- */
 public class MainActivity extends Activity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initToolbar();
     }
+
+    private void initToolbar() {
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        toolbar.setTitle(R.string.app_name);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                return false;
+            }
+        });
+        toolbar.inflateMenu(R.menu.menu);
+
+    }
+
 }
